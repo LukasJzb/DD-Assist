cf9f0b71b05a87640f0a2628e5f1abcca11931eb
Lukas Jarzombek <lukas.jarzombek@yahoo.de>
2023-07-25 19:28:31 +0200

    Initial commit

---
+++ app/src/main/java/com/hsworms_project/dd_assist/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.hsworms_project.dd_assist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navView.background = null
+        navView.menu.getItem(2).isEnabled = false
+        replaceFragment(HomeFragment())
+
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.miHome -> replaceFragment(HomeFragment())
+                R.id.miChar -> replaceFragment(CharFragment())
+                R.id.miNote -> replaceFragment(NoteFragment())
+                R.id.miDice -> replaceFragment(DiceFragment())
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.coordinatorLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
